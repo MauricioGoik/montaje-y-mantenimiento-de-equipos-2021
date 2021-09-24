d89fafe43213eb021f030b82f70fc67d3774271b
MauricioGoik <91250028+MauricioGoik@users.noreply.github.com>
2021-09-24 13:11:10 +0200

    Update Powershell ise menu.ps1

--- Practica 1/Powershell ise menu.ps1
+++ Practica 1/Powershell ise menu.ps1
@@ -10,17 +10,34 @@ $valor = [convert]::ToString($valor,2)
 'el valor es' + $valor
 
 
-write-host'|___________________________|'
-Write-Host'|                           |'
-Write-Host'|                           |'
-Write-Host'|          menú             |'
-Write-Host'|                           |'
-write-host'|___________________________|'
+write-host"|___________________________|"
+Write-host"|                           |"
+Write-host"|                           |"
+Write-host"|          menú             |"
+Write-host"|                           |"
+write-host"|___________________________|"
 write-host " 1. Convierte de binario a decimal"
 write-host " 2. Convierte de hexadecimal a binario"
 write-host " 3. Convierte de octal a hexadecimal"
 write-host " 4. Convierte de decimal a octal"
 $option = read-host "selecciona operacion "
-if($var -eq 1)
-{$var=[convert]::toint32($var,2)
+
+if($option -eq 1)
+{
+$binario = read-host "dime el numero binario"
+$valor=[convert]::ToInt32($binario,2)
+"el numero en decimal es..."+ $valor
+}
+if($option -eq 2)
+{
+$hexa = read-host "dime el numero en hexadecimal"
+$valor = [convert]::ToInt32($hexa, 16)
+$valor = [convert]::ToString($valor,2)
+"el valor en binario es..." + $valor
+}
+if($option -eq 3)
+{
+$octal = read-host "dime el numero en octal"
+$valor = [convert]::ToString($octal,8)
+ "numero en octal es..." + $valor
 }
